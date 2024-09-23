2b720d164c2e9e093ad32eed05983b70101ecafc
Josehp Herrera <u20211e098@upc.edu.pe>
2024-09-22 23:28:56 -0500

    feat: Client and Store entites were added

---
+++ src/main/java/com/example/finappapirest/finances/domain/model/aggregates/Account.java
@@ -0,0 +1,13 @@
+package com.example.finappapirest.finances.domain.model.aggregates;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
--- src/main/java/com/example/finappapirest/finances/domain/model/aggregates/Client.java
+++ src/main/java/com/example/finappapirest/finances/domain/model/aggregates/Client.java
@@ -1,14 +1,22 @@
 package com.example.finappapirest.finances.domain.model.aggregates;
 
 import com.example.finappapirest.shared.domain.model.entities.AuditableModel;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class Client extends AuditableModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String email;
+    private String names;
+    private String paternSurname;
+    private String maternSurname;
+    private String dni;
+    private String phone;
+    private String photo;
+
+    @ManyToOne
+    private Store store;
+    private Long userId;
 }
